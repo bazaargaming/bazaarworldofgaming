fdc768a9da7e449364e56975234255238ff631b1
Ben Levy <bllevy2@ubuntu.(none)>
2014-04-06 17:58:13 -0500

    added json_controller

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -5,5 +5,6 @@ class JsonController < ActionController::Base
 	def index
    		@game = Game.find(params[:id])
     	@json = PriceHistoryHelper.get_sales_histories(@game)
+    	@body = render json: @json
 	end
 end
