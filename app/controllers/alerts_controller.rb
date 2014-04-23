6132e60a944a43ca96032ec9468c54b0ff47206e
Ben Levy <bllevy2@ubuntu.(none)>
2014-04-22 23:39:14 -0500

    changed price history table to scrollable, added in check for whether the modal should be edit or create new alert

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,7 +1,14 @@
 class AlertsController < ApplicationController
 	def create
-		flash[:success] = "Alert was successfully created!"
-		redirect root_path
+		
+		@alert =Game.find(params[:game][:id]).alerts.create({:threshold => params[:alert][:threshold],
+							:user_id => params[:user][:id]})
+		if @alert 
+			flash[:success] = "Alert was successfully created!"
+		else
+			flash[:error] = "Alert was unable to be created!"
+		end
+		redirect_to :back
 	end
 	
 
