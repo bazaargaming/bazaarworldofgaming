f13955a41d5d05011ad49d0d20a3b823d3838c9e
cloroxod <zhuoyuan35@gmail.com>
2014-05-02 17:33:46 -0500

    changed html for alerts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -22,4 +22,21 @@ module UsersHelper
 
 		return arrays
 	end
+	def self.get_alerts_in_groups(user)
+		size = user.alerts.size
+		num_of_arrays = size/6
+		num_of_arrays = size % 6 == 0 ? num_of_arrays : num_of_arrays + 1
+		arrays = Array.new
+
+		for n in 0..num_of_arrays
+			arrays[n] = Array.new
+		end
+
+		user.alerts.each_with_index do |alert, i|
+			array = arrays[i/6]
+			array.push(alert)
+		end
+
+		return arrays
+	end
 end
