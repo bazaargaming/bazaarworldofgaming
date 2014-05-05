c7e0a21aa5f4fe15237a17f12eb89beb6f03db5e
Mikeb924 <mblasingame24@gmail.com>
2014-05-04 18:29:53 -0700

    Fixed method name change in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,11 +68,11 @@ end
 	SteamHelper.extract_page_info("http://store.steampowered.com/app/242050/")
 	SteamHelper.extract_page_info("http://store.steampowered.com/app/265930/")
 
-	GmgHelper.getSalePageInfo("http://www.greenmangaming.com/s/us/en/pc/games/shooter/bioshock-infinite-na/")
-	GmgHelper.getSalePageInfo("http://www.greenmangaming.com/s/us/en/pc/games/rpgs/elder-scrolls-v-skyrim/")
-	GmgHelper.getSalePageInfo("http://www.greenmangaming.com/s/us/en/pc/games/strategy/sid-meiers-civilization-v/")
-	GmgHelper.getSalePageInfo("http://www.greenmangaming.com/s/us/en/pc/games/simulation/goat-simulator/")
-	GmgHelper.getSalePageInfo("http://www.greenmangaming.com/s/us/en/pc/games/action/assassins-creed-iv-black-flag-na/")
+	GmgHelper.get_sale_page_info("http://www.greenmangaming.com/s/us/en/pc/games/shooter/bioshock-infinite-na/")
+	GmgHelper.get_sale_page_info("http://www.greenmangaming.com/s/us/en/pc/games/rpgs/elder-scrolls-v-skyrim/")
+	GmgHelper.get_sale_page_info("http://www.greenmangaming.com/s/us/en/pc/games/strategy/sid-meiers-civilization-v/")
+	GmgHelper.get_sale_page_info("http://www.greenmangaming.com/s/us/en/pc/games/simulation/goat-simulator/")
+	GmgHelper.get_sale_page_info("http://www.greenmangaming.com/s/us/en/pc/games/action/assassins-creed-iv-black-flag-na/")
 
 
 	AmazonHelper.parse_amazon_site
